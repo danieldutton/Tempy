185e12315b38c9fc96431774fe9f1126d407696b
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-04 16:50:44 +0100

    Initial Commit

---
+++ Tempy/TempyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempyApp.swift
+//  Tempy
+//
+//  Created by Daniel Dutton on 04/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TempyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
